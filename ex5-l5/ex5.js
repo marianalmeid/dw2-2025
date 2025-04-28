56651af31f8837aa1e90ec440bdadbd1fe6fd3b4
Mariana <163425141+marianalmeid@users.noreply.github.com>
2025-04-28 20:50:05 -0300

    lista555

--- ex5-l5/ex5.js
+++ ex5-l5/ex5.js
@@ -2,8 +2,7 @@ function boasVindas(nome) {
     alert('Seja bem-vindo ' + nome);
 }
 
-document.getElementById('btn-boas-vindas').onclick = function() {
+document.getElementById('btn-boas-vindas').addEventListener('click', function () {
     const nome = document.getElementById('nome-completo').value;
     boasVindas(nome);
-
-};
+});
